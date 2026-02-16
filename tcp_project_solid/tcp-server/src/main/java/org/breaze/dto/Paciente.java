6b2769d5d49c01ff90f84bff091a1f4a09f86e6e
Sebastian Villaneda Gutierrez <sebastian.villanedag@autonoma.edu.co>
2026-02-16 13:06:22 -0500

    Se crea la clase paciente

---
+++ tcp_project_solid/tcp-server/src/main/java/org/breaze/dto/Paciente.java
@@ -0,0 +1,90 @@
+package org.breaze.dto;
+
+
+public class Paciente {
+
+
+    private String documentoId;
+    private String nombre;
+    private String apellido;
+    private String edad;
+    private String correo;
+    private String genero;
+    private String ciudad;
+    private String pais;
+
+    public Paciente(String documentoId, String nombre, String apellido, String edad, String correo, String ciudad, String genero, String pais) {
+        this.documentoId = documentoId;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.edad = edad;
+        this.correo = correo;
+        this.ciudad = ciudad;
+        this.genero = genero;
+        this.pais = pais;
+    }
+
+    public void setDocumentoId(String documentoId) {
+        this.documentoId = documentoId;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public void setEdad(String edad) {
+        this.edad = edad;
+    }
+
+    public void setPais(String pais) {
+        this.pais = pais;
+    }
+
+    public void setCiudad(String ciudad) {
+        this.ciudad = ciudad;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getDocumentoId() {
+        return documentoId;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public String getEdad() {
+        return edad;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public String getCiudad() {
+        return ciudad;
+    }
+
+    public String getPais() {
+        return pais;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+}
